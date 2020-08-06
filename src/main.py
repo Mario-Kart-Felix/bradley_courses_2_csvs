37eecaf1311918a501b5486f26348c2f33501c25
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-08-06 15:48:21 -0500

    get_url_l() works

--- src/main.py
+++ src/main.py
@@ -1,12 +1,45 @@
 import requests 
+
+from sms.testing_utils import testing_utlils as tu
+from sms.logger        import txt_logger
+from sms.logger        import logger
    
+   
+   
+URL_TXT_FILE_PATH = 'URLs.txt'
+   
+   
+   
+def get_url_l():   
+    raw_lines = txt_logger.read(URL_TXT_FILE_PATH)
+    
+    url_l = []
+    
+    for line in raw_lines:
+        if line != "":
+            url_l.append(line)
+    return url_l
+   
+   
+   
+if __name__ == '__main__':
+    
+    url_l = get_url_l()
+    tu.p_print(url_l)
+    
+    
+    
+    
+    
+    
+    
 # Making a get request 
 # response = requests.get('https://www.bradley.edu/academic/undergradcat/20202021/cfa-artcourses.dot') 
-response = requests.get('https://www.bradley.edu/academic/undergradcat/20202021/turner-entrepreneur-courses.dot') 
-    
-# prinitng request text 
-print(type(response.text)) 
-print(response.text) 
+# response = requests.get('https://www.bradley.edu/academic/undergradcat/20202021/turner-entrepreneur-courses.dot') 
+#     
+# # prinitng request text 
+# print(type(response.text)) 
+# print(response.text) 
 
  
 # from bs4 import BeautifulSoup
