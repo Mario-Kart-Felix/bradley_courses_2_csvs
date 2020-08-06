fff48fce42c5e9490be15360713b7b8090549601
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-08-06 16:32:49 -0500

    gets title with soup

--- src/Courses_Page.py
+++ src/Courses_Page.py
@@ -1,18 +1,32 @@
 import requests 
+from bs4 import BeautifulSoup
+
 
 
 class Courses_Page:
     def __init__(self, url):
-        self.response = requests.get(url)
+        self.page = requests.get(url)
         
-        self.raw_html_str = self.response.text
+        self.raw_html_str = self.page.text
         self.title = self.get_title()
-        print(self.title)
+#         print(self.title)
         
         
         
     def get_title(self):
-        return self.raw_html_str.split('<title>')[1].split('</title>')[0]
+#         return self.raw_html_str.split('<title>')[1].split('</title>')[0]
+
+        soup = BeautifulSoup(self.page.content, 'html.parser')
+#         print(soup.prettify())
+#         results = soup.find(id='title')
+#         results = soup.t
+        raw_title = soup.find('title').get_text()
+#         print(raw_title)
+#         job_elems = soup.find_all('section', class_='title')
+#         job_elems = soup.find_all('title')
+        return raw_title.split(' | ')[0]
+        
+#         job_elems = results.find_all('section', class_='card-content')
     
     
     def print_me(self):
